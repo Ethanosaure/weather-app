cd7e8814f09763849f5aa392e27dfd99694d96aa
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-05-23 16:58:36 +0200

    correction

--- js/fetch-temp.js
+++ js/fetch-temp.js
@@ -48,7 +48,9 @@ export async function fetch_weather() {
     if (weather.list[0].weather[0].main === "Clear") {
       document.body.style.backgroundImage = "url(assets/soleil.jpg)";
     }
-    document.body.style.backgroundImage = "url(assets/venteux.jpg)";
+    if (weather.list[0].weather[0].main === "") {
+      document.body.style.backgroundImage = "url(assets/venteux.jpg)";
+    }
     // append
     div.appendChild(day);
     div.appendChild(humidity);
